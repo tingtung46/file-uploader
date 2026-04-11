6d2c1c0ac30b5fcdc8924bd9cb24c0923bcf2432
Rizma Ernawanti <132725436+tingtung46@users.noreply.github.com>
2026-04-11 09:07:52 +0700

    Add email validation and refine welcome page

--- config/validation.js
+++ config/validation.js
@@ -1,6 +1,20 @@
 import { body } from "express-validator";
+import { prisma } from "../lib/prisma.js";
 
 const validateUser = [
+  body("email").custom(async (value) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: value,
+      },
+    });
+
+    if (user) {
+      throw new Error("E-mail already in use");
+    }
+
+    return true;
+  }),
   body("password")
     .isLength({ min: 5 })
     .withMessage("Password length minimal 5 characters"),
@@ -9,9 +23,9 @@ const validateUser = [
 
     if (!matchedPassword) {
       throw new Error("Password doesn't match");
-    } else {
-      return true;
     }
+
+    return true;
   }),
 ];
 
